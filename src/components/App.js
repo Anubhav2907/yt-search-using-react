02e43401756ba1d6f519d4e86572e2db1773042d
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-05-05 22:26:03 +0530

    initiating youtube api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
+import youtube from "../api/youtube";
 import React from "react";
 import SearchBar from "./SearchBar";
-
 class App extends React.Component {
+  onTermSubmit = (term) => {
+    youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+  };
   render() {
     return (
-      <div>
-        <div>Hi</div>
-        <SearchBar></SearchBar>
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit}></SearchBar>
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,31 @@
-import React from 'react';
-class SearchBar extends React.Component{
-    render(){
-        return <div>
-            SearchBar
-        </div>
-    }
+import React from "react";
+class SearchBar extends React.Component {
+  state = { term: "" };
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.term);
+  };
+  render() {
+    return (
+      <div className="ui segment search-bar">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label className="ui label" htmlFor="search">
+              Video Search
+            </label>
+            <input
+              onChange={this.onInputChange}
+              value={this.state.term}
+              type="text"
+              id="search"
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 export default SearchBar;
